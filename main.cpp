2a691c6fac2718249733cc493e6a3e5048415388
lysenko_v <lysenko_v@itstep.academy>
2023-11-07 18:36:11 +0200

    1

--- main.cpp
+++ main.cpp
@@ -1,4 +1,37 @@
 #include <iostream>
-#include <ctime>
+using namespace std;
 
-using namespace std;  
+void computeSum(int* A, int* B, int* C, int size) {
+    for (int i = 0; i < size; i++) {
+        C[i] = A[i] + B[i];
+    }
+}
+
+int main() {
+    int size = 5;
+    int A[] = { 1, 2, 3, 4, 5 };
+    int B[] = { 6, 7, 8, 9, 10 };
+    int C[5];
+
+    computeSum(A, B, C, size);
+
+    cout << "Array A: ";
+    for (int i = 0; i < size; i++) {
+        cout << A[i] << " ";
+    }
+    cout << endl;
+
+    cout << "Array B: ";
+    for (int i = 0; i < size; i++) {
+        cout << B[i] << " ";
+    }
+    cout << endl;
+
+    cout << "Array C (sum of A and B): ";
+    for (int i = 0; i < size; i++) {
+        cout << C[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
